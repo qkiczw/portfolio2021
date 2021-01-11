006ca5e9029a5b4443628b77eeae1e8c87465b94
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-01-11 20:49:57 +0100

    I added gsap code to animate the logo, and I added flag parcel build command to fix css and js paths

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 console.log('Hello World!');
 
 // GSAP vabiables
+const headerLogo = document.querySelector('.header__logo');
 const heroTitle = document.querySelector('.header__hero-title');
 const heroSubTitle = document.querySelector('.header__hero-subtitle');
 const heroButton = document.querySelector('.header__button');
@@ -10,6 +11,8 @@ const heroButton = document.querySelector('.header__button');
 let tl = gsap.timeline();
 
 // Header animation
+tl.fromTo(headerLogo, {x: 40, y: 250, scale: 1.2, opacity:0}, {opacity:1, duration:1, ease: "easeIn"});
+tl.to(headerLogo, {x:0, y:0 , scale:1, duration:1, ease: "easeIn"});
 tl.fromTo( heroTitle, {y: 100, opacity: 0}, {y: 0, opacity: 1, duration: 1, ease: "easeInOut"});
 tl.fromTo( heroSubTitle, {y: 50, opacity: 0}, {y: 0, opacity: 1, duration: 0.5});
 tl.fromTo( heroButton, {y: 50, opacity: 0}, {y: 0, opacity: 1, duration: 0.5});
